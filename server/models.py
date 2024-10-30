8f427da0def5cdda5cd284d644af2f89fe188021
Majak Deng <majakbanks.deng>
2024-10-30 13:45:55 +0300

    modified

--- server/models.py
+++ server/models.py
@@ -7,3 +7,9 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    image = db.Column(db.String)
+    price = db.Column(db.Float)
+    
+    # Remove or modify serialize_rules to include id
+    serialize_rules = ('-created_at', '-updated_at',)  # Don't exclude 'id'
